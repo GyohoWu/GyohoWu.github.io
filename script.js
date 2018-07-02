93dcabab38c84deb4358e45f3c16b01951344f26
GyohoWu <34412598+GyohoWu@users.noreply.github.com>
2018-07-02 09:14:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ $(function(){
     }
   });
 
-  $('.container a').click(function(){
+
+  $('.scroll').click(function(){
+
     var href = $(this).attr('href');
 
     $('html, body').animate({
@@ -35,6 +37,109 @@ $(function(){
   $('.i-2').hover(
     function(){
       $(this).next().css("display","block");
+  });
+
+  $('#t_modal').click(function() {
+
+    $('.open_2').css('display','none');
+    $('.open_2').removeClass('open_2');
+
+    $('.open_3').css('display','none');
+    $('.open_3').removeClass('open_3');
+
+    $('.open_4').css('display','none');
+    $('.open_4').removeClass('open_4');
+
+    var $answer = $('.transfer_modal');
+    if($answer.hasClass('open_1')) {
+      $answer.removeClass('open_1');
+      $answer.css("display", "none");
+
+    } else {
+      $answer.addClass('open_1');
+      $answer.css("display", "block");
+    }
+  });
+
+  $('#c_modal').click(function() {
+
+    $('.open_1').css('display','none');
+    $('.open_1').removeClass('open_1');
+
+    $('.open_3').css('display','none');
+    $('.open_3').removeClass('open_3');
+
+    $('.open_4').css('display','none');
+    $('.open_4').removeClass('open_4');
+
+    var $answer = $('.class_modal');
+    if($answer.hasClass('open_2')) {
+      $answer.removeClass('open_2');
+      $answer.css("display", "none");
+
+    } else {
+      $answer.addClass('open_2');
+      $answer.css("display", "block");
+    }
+  });
+
+  $('#s_modal').click(function() {
+
+    $('.open_1').css('display','none');
+    $('.open_1').removeClass('open_1');
+
+    $('.open_2').css('display','none');
+    $('.open_2').removeClass('open_2');
+
+    $('.open_4').css('display','none');
+    $('.open_4').removeClass('open_4');
+
+    var $answer = $('.study_modal');
+    if($answer.hasClass('open_3')) {
+      $answer.removeClass('open_3');
+      $answer.css("display", "none");
+
+    } else {
+      $answer.addClass('open_3');
+      $answer.css("display", "block");
+    }
+  });
+
+  $('#ch_modal').click(function() {
+
+    $('.open_1').css('display','none');
+    $('.open_1').removeClass('open_1');
+
+    $('.open_2').css('display','none');
+    $('.open_2').removeClass('open_2');
+
+    $('.open_3').css('display','none');
+    $('.open_3').removeClass('open_3');
+
+    var $answer = $('.chat_modal');
+    if($answer.hasClass('open_4')) {
+      $answer.removeClass('open_4');
+      $answer.css("display", "none");
+
+    } else {
+      $answer.addClass('open_4');
+      $answer.css("display", "block");
+    }
+  });
+
+  $('.tranfer-content').click(function(){
+
+    $('.open_1').css('display','none');
+    $('.open_1').removeClass('open_1');
+
+    $('.open_2').css('display','none');
+    $('.open_2').removeClass('open_2');
+
+    $('.open_3').css('display','none');
+    $('.open_3').removeClass('open_3');
+
+    $('.open_4').css('display','none');
+    $('.open_4').removeClass('open_4');
   });
 
 });
